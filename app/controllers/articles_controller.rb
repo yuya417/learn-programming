45e8e42e9c090cd0835b36f52ce0e584a17d9fb4
鎌田裕也 <ykprogramdiv417@gmail.com>
2020-12-17 01:11:46 +0900

    article show を実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,10 +2,31 @@ class ArticlesController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @articles = Article.all.order("id DESC")
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 
   def new
-    
+    @article = current_user.articles.build
+  end
+
+  def create
+    @article = current_user.articles.build(article_params)
+    if @article.save!
+      redirect_to articles_path, notice: '保存されました！'
+    else
+      flash.now[:error] = '保存に失敗しました'
+      render :new
+    end
+  end
+
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :content, images: [])
   end
   
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -19,4 +19,15 @@
 #
 class Article < ApplicationRecord
   belongs_to :user
+
+  has_many_attached :images
+
+  def display_created_at
+    I18n.l(self.created_at, format: :default)
+  end
+
+  def author_name
+    user.username
+  end
+
 end
