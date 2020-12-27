eb59dfa9347912f54a323d260e76e83888d2c73e
鎌田裕也 <ykprogramdiv417@gmail.com>
2020-12-27 23:14:43 +0900

    no message

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,34 +64,16 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  # config.action_mailer.raise_delivery_errors = true
-  # config.action_mailer.delivery_method = :smtp
-  # host = 'learn-programming-yuya.herokuapp.com'
-  # config_action_mailer.default_url_options = { host: host }
-  # ActionMailer::Base.smtp_settings = {
-  #   :address => 'smtp.sendgrid.net',
-  #   :port => '587',
-  #   :authentication => :plain,
-  #   :user_name => ENV['SENDGRID_USERNAME'],
-  #   :password => ENV['SENDGRID_PASSWORD'],
-  #   :domain => 'heroku.com',
-  #   :enable_starttls_auto => true
-  # }
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true 
-  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
-    :enable_starttls_auto => true,
-    :address => "smtp.sendgrid.net",
-    :port => 587,
-    :domain => 'smtp.sendgrid.net',
-    :user_name => "apikey",
-    :password => ENV['SEND_GRID_API_KEY'],
-    :authentication => :plain
+      address: 'smtp.gmail.com',
+      port: 587,
+      domain: 'gmail.com',
+      user_name: ENV['GOOGLE_USER_NAME'],
+      password: ENV['GOOGLE_SECRET_KEY'],
+      authentication: 'plain',
+      enable_starttls_auto: true
   }
-
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
