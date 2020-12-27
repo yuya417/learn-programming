79de6f305084331ac64dd71b7e7bc877a86c6c76
鎌田裕也 <ykprogramdiv417@gmail.com>
2020-12-27 22:33:42 +0900

    no message

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,8 +2,7 @@ class ArticlesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    user_ids = current_user.followings.pluck(:id)
-    @articles = Article.where(user_id: user_ids)
+    @articles = Article.all.order("id DESC")
   end
 
   def show
