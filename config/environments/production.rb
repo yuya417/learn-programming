c878706d49fae00019e1d990dc78835f231313ef
鎌田裕也 <ykprogramdiv417@gmail.com>
2020-12-27 22:24:02 +0900

    no message

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,21 @@ Rails.application.configure do
   #   :enable_starttls_auto => true
   # }
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.sendgrid.net",
+    :port => 587,
+    :domain => 'smtp.sendgrid.net',
+    :user_name => "apikey",
+    :password => ENV['SEND_GRID_API_KEY'],
+    :authentication => :plain
+  }
+
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
