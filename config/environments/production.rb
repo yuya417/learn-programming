76969af00e1c8ca8de4583373a3a4b11161e6d66
鎌田裕也 <ykprogramdiv417@gmail.com>
2020-12-28 12:44:26 +0900

    no message

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,17 +89,17 @@ Rails.application.configure do
   #   :enable_starttls_auto => true
   # }
 
-  config.action_mailer.default_url_options = { :host => 'https://learn-programming-yuya.herokuapp.com' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-  :enable_starttls_auto => true,
-  :address => "smtp.sendgrid.net",
-  :port => 587,
-  :domain => "heroku.com",
-  :authentication => :plain,
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SENDGRID_PASSWORD'],
-}
+#   config.action_mailer.default_url_options = { :host => 'https://learn-programming-yuya.herokuapp.com' }
+#   config.action_mailer.delivery_method = :smtp
+#   config.action_mailer.smtp_settings = {
+#   :enable_starttls_auto => true,
+#   :address => "smtp.sendgrid.net",
+#   :port => 587,
+#   :domain => "heroku.com",
+#   :authentication => :plain,
+#   :user_name => ENV['SENDGRID_USERNAME'],
+#   :password => ENV['SENDGRID_PASSWORD'],
+# }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
