e4d36f8efd4fd60489ef11a5a449ce87c2be7d41
鎌田裕也 <ykprogramdiv417@gmail.com>
2020-12-28 12:21:15 +0900

    no message

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,19 +75,31 @@ Rails.application.configure do
   #   :password => ENV['SENDGRID_PASSWORD'],
   # }
 
-  config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.delivery_method = :smtp
+  # host = 'learn-programming-yuya.herokuapp.com'
+  # config.action_mailer.default_url_options = { host: host }
+  # ActionMailer::Base.smtp_settings = {
+  #   :address        => 'smtp.sendgrid.net',
+  #   :port           => '587',
+  #   :authentication => :plain,
+  #   :user_name      => ENV['SENDGRID_USERNAME'],
+  #   :password       => ENV['SENDGRID_PASSWORD'],
+  #   :domain         => 'heroku.com',
+  #   :enable_starttls_auto => true
+  # }
+
+  config.action_mailer.default_url_options = { :host => 'https://learn-programming-yuya.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
-  host = 'learn-programming-yuya.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
-  }
+  config.action_mailer.smtp_settings = {
+  :enable_starttls_auto => true,
+  :address => "smtp.sendgrid.net",
+  :port => 587,
+  :domain => "heroku.com",
+  :authentication => :plain,
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+}
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
